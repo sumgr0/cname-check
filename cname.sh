c9e7556881f95b7dace332a18d3eeb28b78b2970
sumgr0 <sumit.grover@gmail.com>
2019-09-20 13:38:21 +0530

    First commit

---
+++ cname.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+while read LINE; do
+    cname=`dig $LINE CNAME +short`
+
+    tld_host=`echo $LINE | awk -F '[.:]' '{print $(NF-1)}'`
+    tld_service=`echo $cname | awk -F '[.:]' '{print $(NF-2)}'`
+    if [ "$cname" = " "]; then
+        echo "$LINE" >> no_cname
+    else 
+        if [ "$tld_host" = "$tld_service" ]; then
+            echo "$LINE" >> host_match
+        else
+            echo "$LINE: $cname" >> cname_out
+        fi
+    fi
+done < $1
