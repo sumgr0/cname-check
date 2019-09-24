5a9c776d4fac590ec4803a3900a63d0776aed7d5
sumgr0 <sumit.grover@gmail.com>
2019-09-24 14:18:12 +0530

    Update cname.sh

--- cname.sh
+++ cname.sh
@@ -1,43 +1,24 @@
 #!/bin/bash
 
+nc=0
+hm=0
+co=0
 while read LINE; do
     echo "no_cname: $nc; host_match: $hm; cname_out: $co"
     cname=`dig $LINE CNAME +short`
 
     if [ -z "$cname" ]; then
         echo "$LINE" >> no_cname
-<<<<<<< HEAD
-<<<<<<< HEAD
         ((nc++))
-=======
-        nc= `wc -l < no_cname`
->>>>>>> parent of d4bb067... test-4
-=======
-        (($nc++))
->>>>>>> parent of 6cae205... test-5
     else
         tld_host=`echo $LINE | awk -F '[.:]' '{print $(NF-1)}'`
         tld_service=`echo $cname | awk -F '[.:]' '{print $(NF-2)}'`
         if [ "$tld_host" = "$tld_service" ]; then
             echo "$LINE" >> host_match
-<<<<<<< HEAD
-<<<<<<< HEAD
             ((hm++))
         else
             echo "$LINE,$cname" >> cname_out
             ((co++))
-=======
-            hm=`wc -l < host_match`
-        else
-            echo "$LINE,$cname" >> cname_out
-            co=`wc -l < cname_out`
->>>>>>> parent of d4bb067... test-4
-=======
-            (($hm++))
-        else
-            echo "$LINE,$cname" >> cname_out
-            (($co++))
->>>>>>> parent of 6cae205... test-5
         fi
     fi
 done < $1 #file to check
