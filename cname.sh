37556f01b768dbeb06dda151bae727cb971532f2
sumgr0 <sumit.grover@gmail.com>
2019-09-24 14:16:17 +0530

    Update cname.sh

--- cname.sh
+++ cname.sh
@@ -7,15 +7,20 @@ while read LINE; do
     if [ -z "$cname" ]; then
         echo "$LINE" >> no_cname
 <<<<<<< HEAD
+<<<<<<< HEAD
         ((nc++))
 =======
         nc= `wc -l < no_cname`
 >>>>>>> parent of d4bb067... test-4
+=======
+        (($nc++))
+>>>>>>> parent of 6cae205... test-5
     else
         tld_host=`echo $LINE | awk -F '[.:]' '{print $(NF-1)}'`
         tld_service=`echo $cname | awk -F '[.:]' '{print $(NF-2)}'`
         if [ "$tld_host" = "$tld_service" ]; then
             echo "$LINE" >> host_match
+<<<<<<< HEAD
 <<<<<<< HEAD
             ((hm++))
         else
@@ -27,6 +32,12 @@ while read LINE; do
             echo "$LINE,$cname" >> cname_out
             co=`wc -l < cname_out`
 >>>>>>> parent of d4bb067... test-4
+=======
+            (($hm++))
+        else
+            echo "$LINE,$cname" >> cname_out
+            (($co++))
+>>>>>>> parent of 6cae205... test-5
         fi
     fi
 done < $1 #file to check
