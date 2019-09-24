981b87f7602c9542b449efc7c4a27296270eff2e
sumgr0 <sumit.grover@gmail.com>
2019-09-24 10:15:18 +0530

    test-2

--- cname.sh
+++ cname.sh
@@ -5,16 +5,17 @@ while read LINE; do
 
     if [ -z "$cname" ]; then
         echo "$LINE" >> no_cname
-        wc -l < no_cname
+        nc= (wc -l < no_cname)
     else
         tld_host=`echo $LINE | awk -F '[.:]' '{print $(NF-1)}'`
         tld_service=`echo $cname | awk -F '[.:]' '{print $(NF-2)}'`
         if [ "$tld_host" = "$tld_service" ]; then
             echo "$LINE" >> host_match
-            wc -l < host_match
+            hm=(wc -l < host_match)
         else
             echo "$LINE,$cname" >> cname_out
-            wc -l < cname_out
+            co=(wc -l < cname_out)
         fi
     fi
+    echo "no_cname: $nc; host_match: $hm; cname_out: $co"
 done < $1 #file to check
